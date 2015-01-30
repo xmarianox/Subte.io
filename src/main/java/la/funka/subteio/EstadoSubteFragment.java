c8491cfa8eaf593b0162b2377772828d5d87c97b
Mariano Molina <mariano@contenidos-digitales.com>
2015-01-30 17:47:26 -0300

    Clean proyect

--- src/main/java/la/funka/subteio/EstadoSubteFragment.java
+++ src/main/java/la/funka/subteio/EstadoSubteFragment.java
@@ -4,7 +4,6 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,8 +12,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
-import com.pnikosis.materialishprogress.ProgressWheel;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
