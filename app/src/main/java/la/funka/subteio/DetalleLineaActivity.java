34c19d2e25c5a1a35f6c954b72195cdcf6ce626c
Mariano Molina <xsin.crueldadx@gmail.com>
2015-08-02 16:24:21 -0300

    Add material animations

--- app/src/main/java/la/funka/subteio/DetalleLineaActivity.java
+++ app/src/main/java/la/funka/subteio/DetalleLineaActivity.java
@@ -1,11 +1,15 @@
 package la.funka.subteio;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
+import android.transition.Slide;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,13 +20,20 @@ import android.widget.TextView;
  * Twitter: @xsincrueldadx
  */
 public class DetalleLineaActivity extends AppCompatActivity {
+    private static final String TAG = "DetalleLineaActivity";
+    private static CollapsingToolbarLayout collapsingToolbarLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initActivityTransitions();
         setContentView(R.layout.activity_detalle_linea);
 
         initToolbar();
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().add(R.id.container, new DetalleLineaFragment()).commit();
@@ -39,6 +50,23 @@ public class DetalleLineaActivity extends AppCompatActivity {
         }
     }
 
+    private void initActivityTransitions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Slide transition = new Slide();
+            transition.excludeTarget(android.R.id.statusBarBackground, true);
+            getWindow().setEnterTransition(transition);
+            getWindow().setReenterTransition(transition);
+        }
+    }
+
+    private void applyPalette(Palette palette) {
+        int primaryDark = getResources().getColor(R.color.primary_dark);
+        int primary = getResources().getColor(R.color.primary);
+        collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
+        collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
+        supportPostponeEnterTransition();
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -52,7 +80,10 @@ public class DetalleLineaActivity extends AppCompatActivity {
             View rootView = inflater.inflate(R.layout.fragment_detalle_linea, container, false);
 
             Intent intent = getActivity().getIntent();
+            String linea = "Linea " + intent.getStringExtra("NOMBRE_LINEA");
             String lineaName = "Pantalla de detalle de la linea " + intent.getStringExtra("NOMBRE_LINEA");
+
+            collapsingToolbarLayout.setTitle(linea);
 
             TextView textView = (TextView) rootView.findViewById(R.id.detalle_linea_name);
             textView.setText(lineaName);
---
+++ app/src/main/java/la/funka/subteio/ui/SquareImageView.java
@@ -0,0 +1,32 @@
+package la.funka.subteio.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Mariano Molina on 2/8/15.
+ * Twitter: @xsincrueldadx
+ */
+public class SquareImageView extends ImageView {
+
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttrs) {
+        super(context, attrs, defStyleAttrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        setMeasuredDimension(width, width);
+    }
+}
