d57e6fb25666942e8a49e3f15d95750c9be71df2
Mariano Molina <xsin.crueldadx@gmail.com>
2015-09-26 16:08:24 -0300

    :bug: GoogleMaps Fix, ya se puede visualizar el mapa.

--- app/src/main/java/la/funka/subteio/fragments/MapaSubteFragment.java
+++ app/src/main/java/la/funka/subteio/fragments/MapaSubteFragment.java
@@ -2,12 +2,15 @@ package la.funka.subteio.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -22,6 +25,7 @@ public class MapaSubteFragment extends Fragment {
     private static final String LOG_TAG = MapaSubteFragment.class.getSimpleName();
 
     private Realm realm;
+    private SupportMapFragment supportMapFragment;
     private GoogleMap map;
 
     public MapaSubteFragment() {
@@ -30,16 +34,19 @@ public class MapaSubteFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_map, container, false);
-        if (savedInstanceState == null) {
-            setUpMapIfNeeded();
-        }
-        return v;
+        return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        FragmentManager fm = getChildFragmentManager();
+        supportMapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
+        if (supportMapFragment == null) {
+            supportMapFragment = SupportMapFragment.newInstance();
+            fm.beginTransaction().replace(R.id.map_container, supportMapFragment).commit();
+        }
 
         // configure realm
         RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(getActivity())
@@ -52,24 +59,15 @@ public class MapaSubteFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-
-        setUpMapIfNeeded();
+        if (map == null) {
+            map = supportMapFragment.getMap();
+            map.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         realm.close();
-    }
-
-    private void setUpMapIfNeeded() {
-        if (map != null) {
-            return;
-        }
-        map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        if (map == null) {
-            return;
-        }
-        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
     }
 }
