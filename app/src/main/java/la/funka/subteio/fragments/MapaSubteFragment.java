fdfa07207d13878ee61dcbe87c3adc8f07ed24fc
Mariano Molina <xsin.crueldadx@gmail.com>
2015-09-26 17:04:25 -0300

    :earth_americas: El mapa ya muestra Markers nuevamente

--- app/src/main/java/la/funka/subteio/fragments/MapaSubteFragment.java
+++ app/src/main/java/la/funka/subteio/fragments/MapaSubteFragment.java
@@ -3,10 +3,12 @@ package la.funka.subteio.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -14,7 +16,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import io.realm.RealmResults;
 import la.funka.subteio.R;
+import la.funka.subteio.model.SubwayStation;
 
 /**
  * Created by Mariano Molina on 02/09/2015.
@@ -52,6 +56,7 @@ public class MapaSubteFragment extends Fragment {
         RealmConfiguration realmConfiguration = new RealmConfiguration.Builder(getActivity())
                 .name("stations.realm")
                 .build();
+        // Realm.deleteRealm(realmConfiguration);
         // Clear the real from last time
         realm = Realm.getInstance(realmConfiguration);
     }
@@ -61,7 +66,57 @@ public class MapaSubteFragment extends Fragment {
         super.onResume();
         if (map == null) {
             map = supportMapFragment.getMap();
-            map.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+            /*map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            map.setMyLocationEnabled(true);
+            // Centramos el mapa en BUENOS AIRES
+            LatLng BUE = new LatLng(-34.6160275,-58.4333203);
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(BUE, 11));*/
+        }
+
+        /**
+         * [SubwayStation = [
+         *  {id_station:70},
+         *  {station_name:Plaza de Mayo},
+         *  {id_line:1},
+         *  {line_name:A},
+         *  {lon:-58.370968499724384},
+         *  {lat:-34.60881030966099},
+         *  {address:Hipólito Yrigoyen 300},
+         *  {elevador:false},
+         *  {escalator:false},
+         *  {toilets:true},
+         *  {consultation:true},
+         *  {wifi:false},
+         *  {bus_lines:7, 8, 22, 28, 29, 33, 50, 56, 64, 86, 91, 93, 102, 105, 111, 126, 129, 130, 143, 146, 152, 159, 195}
+         * ],
+         * */
+
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.setMyLocationEnabled(true);
+        // Centramos el mapa en BUENOS AIRES
+        LatLng BUE = new LatLng(-34.6160275,-58.4333203);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(BUE, 11));
+
+        // Add points
+        parseGeoData(map);
+
+    }
+
+    private void parseGeoData(GoogleMap googleMap) {
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        RealmResults<SubwayStation> subwayStations = realm.where(SubwayStation.class).findAll();
+        Log.d(LOG_TAG, subwayStations.toString());
+
+        for (int i = 0; i < subwayStations.size() ; i++) {
+            // station name
+            markerOptions.title(subwayStations.get(i).getStation_name());
+            // station address
+            markerOptions.snippet(subwayStations.get(i).getAddress());
+            // station position.
+            markerOptions.position(new LatLng(subwayStations.get(i).getLat(), subwayStations.get(i).getLon()));
+            // Add Marker
+            googleMap.addMarker(markerOptions);
         }
     }
 
